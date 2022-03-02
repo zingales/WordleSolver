8543c784bf990d9b50cb696db5764fcc54ad9553
G <dont_email_me_please@gmail.com>
2022-03-02 18:47:12 -0500

    added some menu options

--- main.py
+++ main.py
@@ -35,8 +35,6 @@ def word_guess_to_value(word, hints):
             correct_guesses+=word[i]
         if hints[i].lower() == 'g':
             correct_guesses+=word[i]
-        # print(i, hints[i], word[i], correct_guesses)
-    # print(correct_guesses)
     return convert_word_to_value(correct_guesses)
 
 
@@ -51,15 +49,8 @@ def word_guess_to_non_values(word, hints):
     return incorrect_values
 
 
-if __name__ == '__main__':
-    # some_words = {55737073: 'windy', 85822913: 'wiser', 657549323: 'wispy', 12876205: 'witch', 933457093: 'witty', 57200363: 'woken', 13754926: 'woman', 75652093: 'women', 124492613: 'woody', 123025837: 'wooer', 658032383: 'wooly', 1796250559: 'woozy', 61631899: 'world', 1408015237: 'worry', 1131980477: 'worst', 73756207: 'would', 85716673: 'wound', 145768667: 'woven', 1569530: 'wrack', 13659974: 'wrath', 3452966: 'wreak', 8632415: 'wreck', 264931601: 'wrest', 85124219: 'wring', 553947893: 'wrist', 90946669: 'write', 270176869: 'wrung', 1762597379: 'wryly', 1308530: 'yacht', 5597482: 'yearn', 10151438: 'yeast', 6356119: 'yield', 243281917: 'young', 448956643: 'youth', 406626: 'zebra', 512647619: 'zesty', 15104954: 'zonal'}
-
-    # all_words_value_to_word_map = some_words
-    all_words_value_to_word_map = generate_numbers_to_words_map(get_all_words())
-
-    # print(all_words_value_to_word_map)
+def let_us_play(all_words_value_to_word_map):
     availible_word_values = list(all_words_value_to_word_map.keys())
-    # print(availible_word_values)
     while len(availible_word_values) != 1:
         guess = input("what word was guessed\n")
         response = input("what did the system respond. b for blank, y for yellow, g for green\n")
@@ -79,4 +70,19 @@ if __name__ == '__main__':
             for value in availible_word_values:
                 print(all_words_value_to_word_map[value])
 
+        next = input("click enter for next guess\n\ts to show what words are availilbe\n\tq to quit\n\tr to restart with a new word\n")
+        if next.lower() == 's':
+            for value in availible_word_values:
+                print(all_words_value_to_word_map[value])
+        if next.lower() == 'q':
+            return 'q'
+        if next.lower() == 'r':
+            return 'r'
+
     print("the word is", all_words_value_to_word_map[availible_word_values[0]])
+
+
+if __name__ == '__main__':
+    all_words_value_to_word_map = generate_numbers_to_words_map(get_all_words())
+    while (let_us_play(all_words_value_to_word_map) == 'r'):
+        pass
