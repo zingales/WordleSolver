fc0ee2f78f1ef64633825c7cfd930d37903f0b09
G <dont_email_me_please@gmail.com>
2022-03-03 12:30:51 -0500

    crappy patch for fixing anagrams

--- main.py
+++ main.py
@@ -22,10 +22,18 @@ def convert_word_to_value(word):
 def generate_numbers_to_words_map(words):
     numbers_to_words_map = dict()
 
-    for word in words:
-        numbers_to_words_map[convert_word_to_value(word)]=word.strip()
+    anagrams = dict()
+    for raw_word in words:
+        word = raw_word.strip()
+        value = convert_word_to_value(word)
+        if value in numbers_to_words_map:
+            anagram_words = anagrams.get(value, set())
+            anagram_words.add(word)
+            anagram_words.add(numbers_to_words_map[value])
+            anagrams[value]=anagram_words
+        numbers_to_words_map[value]=word
 
-    return numbers_to_words_map
+    return numbers_to_words_map, anagrams
 
 
 def word_guess_to_value(word, hints):
@@ -73,9 +81,11 @@ def get_green_and_yellow_tuples(word, hints):
     return greens, yellows
 
 
-def let_us_play(all_words_value_to_word_map, letter_spot_value_map):
-    print("now let's start")
+def let_us_play(all_words):
+    all_words_value_to_word_map, anagrams = generate_numbers_to_words_map(all_words)
+    letter_spot_value_map = generate_string_location_map(all_words)
     availible_word_values = set(all_words_value_to_word_map.keys())
+    print("now let's start")
     while len(availible_word_values) > 1:
         guess = input("what word was guessed\n").lower()
         if len(guess) < 5:
@@ -110,8 +120,6 @@ def let_us_play(all_words_value_to_word_map, letter_spot_value_map):
                 if word_value not in availible_word_values:
                     break
                 if word_value % incorrect_value == 0:
-                    if word_value == 351654:
-                        print("removing due to incorrect value", incorrect_value)
                     availible_word_values.remove(word_value)
 
         greens, yellows = get_green_and_yellow_tuples(guess, response)
@@ -120,12 +128,16 @@ def let_us_play(all_words_value_to_word_map, letter_spot_value_map):
             availible_word_values = availible_word_values.intersection(letter_spot_value_map[letter][index])
 
         for letter,index in yellows:
-            availible_word_values = availible_word_values.difference(letter_spot_value_map[letter][index])
+            values_with_anagrams = set(letter_spot_value_map[letter][index])
+            values = values_with_anagrams.difference(anagrams.keys())
+            availible_word_values = availible_word_values.difference(values)
 
         print("availible words left", len(availible_word_values))
         if len(availible_word_values) < 11:
             for value in availible_word_values:
                 print(all_words_value_to_word_map[value])
+                if value in anagrams:
+                    print(anagrams[value])
 
 
     if len(availible_word_values) == 0:
@@ -138,5 +150,5 @@ def let_us_play(all_words_value_to_word_map, letter_spot_value_map):
 if __name__ == '__main__':
     all_words = get_all_words()
     print("Menu instead of guessing a word type these letters\nclick enter for next guess\n\ts to show what words are availilbe\n\tq to quit\n\tr to restart with a new word\n")
-    while (let_us_play(generate_numbers_to_words_map(all_words), generate_string_location_map(all_words)) == 'r'):
+    while (let_us_play(all_words) == 'r'):
         pass
